593988ea52ec2aa5a49ca21fcbd9001175e1710a
Lina Martinez <lmcastro3501@gmail.com>
2025-07-23 09:23:37 -0700

    Add serializers for User, Tag, Word, Meaning, and Definition models

---
+++ vocabloom/serializers.py
@@ -0,0 +1,47 @@
+from rest_framework import serializers
+from django.contrib.auth.models import User
+from .models import Tag, Word, Meaning, Definition
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id', 'email', 'first_name', 'last_name']
+
+
+class TagSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Tag
+        fields = ['id', 'user', 'name']
+
+
+class DefinitionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Definition
+        fields = ['id', 'definition', 'example']
+
+
+class MeaningSerializer(serializers.ModelSerializer):
+    definitions = DefinitionSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Meaning
+        fields = ['id', 'word', 'part_of_speech', 'definitions']
+
+
+class WordSerializer(serializers.ModelSerializer):
+    meanings = MeaningSerializer(many=True, read_only=True)
+
+    class Meta: 
+        model = Word
+        fields = [
+            'id',
+            'user',
+            'tag',
+            'word',
+            'phonetic',
+            'audio_url',
+            'note',
+            'created_at',
+            'meanings'
+        ]
