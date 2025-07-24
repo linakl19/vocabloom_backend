2b6bc18a0fae6f04c4f60cbf630635ab0c619bce
Lina Martinez <lmcastro3501@gmail.com>
2025-07-23 23:24:16 -0700

    Refactor UserSerializer and add UserRegistrationSerializer for user registration

--- vocabloom/serializers.py
+++ vocabloom/serializers.py
@@ -3,10 +3,28 @@ from django.contrib.auth.models import User
 from .models import Tag, Word, Meaning, Definition
 
 
-# class UserSerializer(serializers.ModelSerializer):
-#     class Meta:
-#         model = User
-#         fields = ['id', 'email', 'first_name', 'last_name']
+class UserRegistrationSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
+    class Meta: 
+        model=User
+        fields=['username', 'email', 'password', 'first_name', 'last_name']
+    
+    def create(self, validated_data):
+        user = User(
+            username=validated_data['username'],
+            email=validated_data['email'],
+            first_name=validated_data['first_name'],
+            last_name=validated_data['last_name']
+        )
+        user.set_password(validated_data['password'])
+        user.save()
+        return user
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'email', 'first_name', 'last_name']
 
 
 class TagSerializer(serializers.ModelSerializer):
