9fcffc60f6df7ab3bb3b8f21a6fd955040e32d84
Lina Martinez <lmcastro3501@gmail.com>
2025-07-23 21:51:10 -0700

    Implement CustomTokenObtainPairView to set access and refresh tokens as secure cookies

--- vocabloom/urls.py
+++ vocabloom/urls.py
@@ -1,13 +1,12 @@
 from rest_framework_simplejwt.views import (
-    TokenObtainPairView,
     TokenRefreshView,
 )
 from django.urls import path
 
-from .views import get_tags
+from .views import get_tags, CustomTokenObtainPairView
 
 urlpatterns = [
-    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/', CustomTokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('tags/', get_tags)
 ]
--- vocabloom/views.py
+++ vocabloom/views.py
@@ -7,6 +7,49 @@ from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated, AllowAny
 from rest_framework.response import Response
 
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+
+
+class CustomTokenObtainPairView(TokenObtainPairView):
+    def post(self, request, *args, **kwargs):
+
+        try:
+            response = super().post(request, *args, **kwargs)
+            tokens = response.data
+
+            access_token = tokens['access']
+            refresh_token = tokens['refresh']
+
+            res = Response()
+
+            res.data = {'success': True}
+
+            res.set_cookie(
+                key="access_token", 
+                value=access_token,
+                httponly=True, 
+                secure=True,
+                samesite='None', 
+                path='/'
+            )
+
+            res.set_cookie(
+                key="refresh_token", 
+                value=refresh_token,
+                httponly=True, 
+                secure=True,
+                samesite='None', 
+                path='/'
+            )
+
+            return res
+        
+        except: 
+            return Response({'success': False})
+        
 
 @api_view(['GET'])
 @permission_classes([IsAuthenticated])
