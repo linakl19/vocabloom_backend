926c337a5f4104c51f8b61ff7c36772b823a1c1a
Lina Martinez <lmcastro3501@gmail.com>
2025-07-23 20:20:42 -0700

    Add get_tags view with permission class and update URL patterns for tag retrieval

--- vocabloom/urls.py
+++ vocabloom/urls.py
@@ -4,8 +4,10 @@ from rest_framework_simplejwt.views import (
 )
 from django.urls import path
 
+from .views import get_tags
 
 urlpatterns = [
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('tags/', get_tags)
 ]
--- vocabloom/views.py
+++ vocabloom/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User
+from .models import Tag
+from .serializers import TagSerializer
 
-# Create your views here.
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated, AllowAny
+from rest_framework.response import Response
+
+
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+def get_tags(request):
+    user = request.user
+    tags = Tag.objects.filter(user=user)
+    serializer = TagSerializer(tags, many=True)
+
+    return Response(serializer.data)
