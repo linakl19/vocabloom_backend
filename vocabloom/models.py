1d7b8d28c2a4c6306fcc9e65d0acf957bda69f7a
Lina Martinez <lmcastro3501@gmail.com>
2025-07-24 19:08:37 -0700

    Remove commented-out User model code from models.py

--- vocabloom/models.py
+++ vocabloom/models.py
@@ -1,17 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-
-# class User(models.Model):
-#     first_name = models.CharField(max_length=100)
-#     last_name = models.CharField(max_length=100)
-#     email = models.EmailField(max_length=254, unique=True)
-#     password = models.CharField(max_length=128)
-
-#     def __str__(self):
-#         return self.first_name
-
-
 class Tag(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='tags')
     name = models.CharField(max_length=50)
