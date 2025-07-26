9d086928f210143ea5fd948a2c286a0e6a37f32c
Lina Martinez <lmcastro3501@gmail.com>
2025-07-25 20:15:30 -0700

    Refactor authentication views to use class-based views and extend schema for better API documentation

--- vocabloom/views.py
+++ vocabloom/views.py
@@ -1,20 +1,36 @@
-from django.shortcuts import render
 from django.contrib.auth.models import User
-from .models import Tag, Word
-from .serializers import TagSerializer, UserRegistrationSerializer, WordSerializer, WordBasicSerializer
-
-from rest_framework.decorators import api_view, permission_classes
+from django.shortcuts import get_object_or_404
+from rest_framework import generics, views, status
 from rest_framework.permissions import IsAuthenticated, AllowAny
 from rest_framework.response import Response
-from rest_framework import status
+from drf_spectacular.utils import extend_schema
+
+from .models import Tag, Word
+from .serializers import (
+    TagSerializer,
+    UserRegistrationSerializer,
+    WordSerializer,
+    WordBasicSerializer,
+    SimpleSuccessSerializer,
+    SimpleRefreshedSerializer,
+    SimpleAuthenticatedSerializer
+)
 
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
 )
 
+# ////////////////////
+# AUTHENTICATION VIEWS
+# ////////////////////
 
 class CustomTokenObtainPairView(TokenObtainPairView):
+    @extend_schema(
+        responses=SimpleSuccessSerializer,
+        tags=['Authentication']
+    )
+        
     def post(self, request, *args, **kwargs):
 
         try:
@@ -51,8 +67,11 @@ class CustomTokenObtainPairView(TokenObtainPairView):
         except: 
             return Response({'success': False})
 
-
 class CustomRefreshTokenView(TokenRefreshView):
+    @extend_schema(
+        responses=SimpleRefreshedSerializer,
+        tags=['Authentication']
+    )
     def post(self, request, *args, **kwargs):
         try:
             refresh_token = request.COOKIES.get('refresh_token')
@@ -80,131 +99,109 @@ class CustomRefreshTokenView(TokenRefreshView):
         except:
             return Response({'refreshed': False})
 
+@extend_schema(responses=SimpleSuccessSerializer, tags=['Authentication'])
+class LogoutView(views.APIView):
+    permission_classes = [IsAuthenticated]
+    serializer_class = SimpleSuccessSerializer
 
-@api_view(['POST'])
-def logout(request):
-    try:
-        res = Response()
-        res.data = {'success': True}
+    def post(self, request):
+        res = Response({'success': True})
         res.delete_cookie('access_token', path='/', samesite='None')
         res.delete_cookie('refresh_token', path='/', samesite='None')
         return res
-    except:
-        return Response({'refreshed': False})
 
+@extend_schema(responses=SimpleAuthenticatedSerializer, tags=['Authentication'])
+class IsAuthenticatedView(views.APIView):
+    permission_classes = [IsAuthenticated]
+    serializer_class = SimpleAuthenticatedSerializer
 
-@api_view(['GET', 'POST'])
-@permission_classes([IsAuthenticated])
-def is_authenticated(request):
-    return Response({'authenticated': True})
+    def get(self, request):
+        return Response({'authenticated': True})
+    def post(self, request):
+        return Response({'authenticated': True})
 
+@extend_schema(request=UserRegistrationSerializer, responses=UserRegistrationSerializer, tags=['Authentication'])
+class RegisterUserView(views.APIView):
+    permission_classes = [AllowAny]
+    serializer_class = UserRegistrationSerializer
 
-@api_view(['POST'])
-@permission_classes([AllowAny])
-def register_user(request):
-    serializer = UserRegistrationSerializer(data=request.data)
-    if serializer.is_valid():
-        serializer.save()
-        return Response(serializer.data)
-    return Response(serializer.errors)
-
-
-@api_view(['GET', 'POST'])
-@permission_classes([IsAuthenticated])
-def tags_list_create(request):
-    if request.method == 'GET':
-        user = request.user
-        tags = Tag.objects.filter(user=user)
-        serializer = TagSerializer(tags, many=True)
-
-        return Response(serializer.data)
-    
-    if request.method == 'POST':
-        serializer = TagSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save(user=request.user)
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-
-@api_view(['GET', 'PATCH', 'DELETE'])
-@permission_classes([IsAuthenticated])
-def tag_detail(request, pk):
-    try:
-        tag = Tag.objects.get(id=pk, user=request.user)
-    except Tag.DoesNotExist:
-        return Response({'detail': 'Not found.'}, status=status.HTTP_404_NOT_FOUND)
-
-    if request.method == 'GET':
-        serializer = TagSerializer(tag)
-        return Response(serializer.data)
-
-    elif request.method == 'PATCH':
-        serializer = TagSerializer(tag, data=request.data, partial=True)
-        if serializer.is_valid():
-            serializer.save(user=request.user)
-            return Response(serializer.data, status=status.HTTP_200_OK)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-    
-    elif request.method == 'DELETE':
-        tag.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
-
-
-@api_view(['GET'])
-@permission_classes([IsAuthenticated])
-def words_by_tag(request, pk):
-    try:
-        tag = Tag.objects.get(id=pk, user=request.user)
-    except Tag.DoesNotExist:
-        return Response({'detail': 'Not found.'}, status=status.HTTP_404_NOT_FOUND)
-    
-    words = Word.objects.filter(tag=tag, user=request.user)
-    serializer = WordBasicSerializer(words, many=True)
-    return Response(serializer.data)
-
-
-
-@api_view(['GET', 'POST'])
-@permission_classes([IsAuthenticated])
-def words_list_create(request):
-    if request.method == 'GET':
-        user = request.user
-        words = Word.objects.filter(user=user)
-        serializer = WordBasicSerializer(words, many=True)
-
-        return Response(serializer.data)
-    
-    elif request.method == 'POST':
-        serializer = WordSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save(user=request.user)
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-
-@api_view(['GET', 'PATCH', 'DELETE'])
-@permission_classes([IsAuthenticated])
-def word_detail(request, pk):
-    try:
-        word = Word.objects.get(pk=pk, user=request.user)
-    except Word.DoesNotExist:
-        return Response({'detail': 'Not found.'}, status=status.HTTP_404_NOT_FOUND)
-
-    if request.method == 'GET':
-        serializer = WordSerializer(word)
-        return Response(serializer.data)
-
-    elif request.method == 'PATCH':
-        if 'note' not in request.data:
-            return Response({'detail': 'Only "note" field can be updated.'}, status=status.HTTP_400_BAD_REQUEST)
-        serializer = WordSerializer(word, data={'note': request.data['note']}, partial=True)
+    def post(self, request):
+        serializer = UserRegistrationSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        return Response(serializer.errors)
 
-    elif request.method == 'DELETE':
-        word.delete()  
-        return Response(status=status.HTTP_204_NO_CONTENT)
+# ////////////
+# TAG VIEWS 
+# ///////////
+
+@extend_schema(tags=['Tags'])
+class TagListCreateView(generics.ListCreateAPIView):
+    permission_classes = [IsAuthenticated]
+    serializer_class = TagSerializer
+
+    def get_queryset(self):
+        return Tag.objects.filter(user=self.request.user)
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+
+@extend_schema(tags=['Tags'])
+class TagDetailView(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = [IsAuthenticated]
+    serializer_class = TagSerializer
+
+    def get_queryset(self):
+        return Tag.objects.filter(user=self.request.user)
+
+    def partial_update(self, request, *args, **kwargs):
+        kwargs['partial'] = True
+        return self.update(request, *args, **kwargs)
+
+# ////////////
+# WORD VIEWS 
+# ///////////
+
+@extend_schema(tags=['Words'])
+class WordsByTagView(generics.ListAPIView):
+    permission_classes = [IsAuthenticated]
+    serializer_class = WordBasicSerializer
+
+    def get_queryset(self):
+        tag_id = self.kwargs['pk']
+        user = self.request.user
+        get_object_or_404(Tag, id=tag_id, user=user)
+        return Word.objects.filter(tag__id=tag_id, user=user)
+
+@extend_schema(tags=['Words'])
+class WordListCreateView(generics.ListCreateAPIView):
+    permission_classes=[IsAuthenticated]
+    serializer_class=WordSerializer
+
+    def get_queryset(self):
+        return Word.objects.filter(user=self.request.user)
+    
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+
+@extend_schema(tags=['Words'])
+class WordDetailView(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes=[IsAuthenticated]
+    serializer_class=WordSerializer
+
+    def get_queryset(self):
+        return Word.objects.filter(user=self.request.user)
+    
+    def partial_update(self, request, *args, **kwargs):
+        instance = self.get_object()
+        note_value = request.data.get('note', None)
+
+        if note_value is None:
+            return Response({'detail': 'PATCH must include a "note" field.'}, status=status.HTTP_400_BAD_REQUEST)
+
+        serializer = self.get_serializer(instance, data={'note': note_value}, partial=True)
+        serializer.is_valid(raise_exception=True)
+        self.perform_update(serializer)
+        return Response(serializer.data)
+
