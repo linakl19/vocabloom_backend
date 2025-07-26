ddb2e15c41ed5d1dca1d1bfa1139afa78d6492e0
Lina Martinez <lmcastro3501@gmail.com>
2025-07-25 20:17:25 -0700

    Refactor URL patterns to use class-based views for authentication and resource endpoints

--- vocabloom/urls.py
+++ vocabloom/urls.py
@@ -1,18 +1,32 @@
 from django.urls import path
 
-from .views import CustomTokenObtainPairView, CustomRefreshTokenView, logout, is_authenticated, register_user, tags_list_create, tag_detail, words_list_create, word_detail, words_by_tag
+from .views import (
+    CustomTokenObtainPairView,
+    CustomRefreshTokenView,
+    IsAuthenticatedView,
+    LogoutView,
+    RegisterUserView,
+    TagListCreateView,
+    TagDetailView,
+    WordsByTagView,
+    WordListCreateView,
+    WordDetailView
+)
 
 urlpatterns = [
+    # Authentication Endpoints
     path('token/', CustomTokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', CustomRefreshTokenView.as_view(), name='token_refresh'),
-    path('logout/', logout), 
-    path('authenticated/', is_authenticated),
-    path('register_user/', register_user),
+    path('logout/', LogoutView.as_view(), name='logout'), 
+    path('authenticated/', IsAuthenticatedView.as_view(), name='is_authenticated'),
+    path('register_user/', RegisterUserView.as_view(), name='register_user'),
 
-    path('tags/', tags_list_create), 
-    path('tags/<int:pk>/', tag_detail),
-    path('tags/<int:pk>/words', words_by_tag),
+    # Tag endpoints
+    path('tags/', TagListCreateView.as_view(), name='tags_list_create'),
+    path('tags/<int:pk>/', TagDetailView.as_view(), name='tag_detail'),
 
-    path('words/', words_list_create),
-    path('words/<int:pk>/', word_detail),
+    #  Word endpoints
+    path('words/', WordListCreateView.as_view(), name='words_list_create'),
+    path('words/<int:pk>/', WordDetailView.as_view(), name='word_detail'),
+    path('tags/<int:pk>/words/', WordsByTagView.as_view(), name='words_by_tag'),
 ]
